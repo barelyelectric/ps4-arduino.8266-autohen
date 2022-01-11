ab5c135f24e71738c699de99ecc1be06add06ac9
skudave <71984727+skudave@users.noreply.github.com>
2022-01-10 22:31:26 -0800

    Add files via upload

---
+++ arduino-micro_kbd-emulator.ino
@@ -0,0 +1,64 @@
+#include <Keyboard.h>
+
+void setup()
+{
+  Keyboard.begin();
+}
+
+void loop()
+{
+    delay(20000);
+	Keyboard.press(KEY_UP_ARROW);
+    delay(25);
+    Keyboard.release(KEY_UP_ARROW);
+    delay(125);
+    Keyboard.press(KEY_RIGHT_ARROW);
+    delay(25);
+    Keyboard.release(KEY_RIGHT_ARROW);
+    delay(125);
+    Keyboard.press(KEY_RIGHT_ARROW);
+    delay(25);
+    Keyboard.release(KEY_RIGHT_ARROW);
+    delay(125);
+    Keyboard.press(KEY_RIGHT_ARROW);
+    delay(25);
+    Keyboard.release(KEY_RIGHT_ARROW);
+    delay(125);
+    Keyboard.press(KEY_RIGHT_ARROW);
+    delay(25);
+    Keyboard.release(KEY_RIGHT_ARROW);
+    delay(125);
+    Keyboard.press(KEY_RIGHT_ARROW);
+    delay(25);
+    Keyboard.release(KEY_RIGHT_ARROW);
+    delay(125);
+    Keyboard.press(KEY_RIGHT_ARROW);
+    delay(25);
+    Keyboard.release(KEY_RIGHT_ARROW);
+    delay(125);
+    Keyboard.press(KEY_RIGHT_ARROW);
+    delay(25);
+    Keyboard.release(KEY_RIGHT_ARROW);
+    delay(125);
+    Keyboard.press(KEY_RETURN);
+    delay(25);
+    Keyboard.release(KEY_RETURN);
+    delay(400);
+    Keyboard.press(KEY_UP_ARROW);
+    delay(25);
+    Keyboard.release(KEY_UP_ARROW);
+    delay(125);
+    Keyboard.press(KEY_UP_ARROW);
+    delay(25);
+    Keyboard.release(KEY_UP_ARROW);
+    delay(125);
+    Keyboard.press(KEY_UP_ARROW);
+    delay(25);
+    Keyboard.release(KEY_UP_ARROW);
+    delay(125);	
+    Keyboard.press(KEY_RETURN);
+    delay(25);
+    Keyboard.release(KEY_RETURN);
+    delay(125);
+    exit(0);
+}
